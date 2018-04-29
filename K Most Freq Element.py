36d8b19ad11d06b6672304795375a9896e8f3455
YiZ0 <38579177+YiZ0@users.noreply.github.com>
2018-04-29 12:25:54 -0500

    Update K Most Freq Element.py

--- K Most Freq Element.py
+++ K Most Freq Element.py
@@ -1,7 +1,13 @@
-def most_k_freq_number(arr):
+def most_k_freq_number(arr, k):
     freq_bucket = {}
-    for i in len(arr):
-        if freq_bucket[i] in None:
+    for i in range(len(arr)):
+        if len(freq_bucket[i]) == 0:
             freq_bucket[i] = arr[i]
         else:
             freq_bucket[i].append(arr[i])
+    s = 0
+
+    if s < k:
+        for i in range(len(freq_bucket), -1, 0):
+            s += 1
+            return(freq_bucket[i])
